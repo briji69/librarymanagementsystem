ddf0034d6a359e514e0b14cc6391d730e4d67c3d
YashuReddy123 <yashus.v.g.p@gmail.com>
2019-10-26 11:08:20 +0530

    added some methods in controller

--- librarymanagementsystem/src/main/java/com/capgemini/librarymanagementsystem/controller/LibrarianController.java
+++ librarymanagementsystem/src/main/java/com/capgemini/librarymanagementsystem/controller/LibrarianController.java
@@ -2,9 +2,13 @@ package com.capgemini.librarymanagementsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capgemini.librarymanagementsystem.dto.BooksInventoryInfo;
 import com.capgemini.librarymanagementsystem.service.LibrarianService;
+import com.capgemini.librarymanagementsystem.util.BookResponse;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -12,4 +16,21 @@ public class LibrarianController {
 
 	@Autowired
 	LibrarianService librarianService;
+	
+	@PostMapping("admin/addLibrarian")
+	public BookResponse addBook(@RequestBody BooksInventoryInfo book) {
+		BookResponse response = new BookResponse();
+		BooksInventoryInfo book1=librarianService.addBook(book);
+		if(book1!=null) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("User registerd Successfully");
+			response.setBook(book1);
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failed");
+			response.setDescription("Unable to Register");
+		}
+		return response;
+	}//end of addLibrarian()
 }
